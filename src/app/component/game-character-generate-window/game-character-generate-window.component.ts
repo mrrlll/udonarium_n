06b814784d97598b9b21a78b0d1342fdbc6484a9
mrrlll <mxrs2d@gmail.com>
2023-07-22 13:57:42 +0900

    一度jsonでキャラ駒データを作成し、xmlへ変換するようにした

--- src/app/component/game-character-generate-window/game-character-generate-window.component.ts
+++ src/app/component/game-character-generate-window/game-character-generate-window.component.ts
@@ -10,7 +10,6 @@ import { PanelOption, PanelService } from 'service/panel.service';
 import { ModalService } from 'service/modal.service';
 
 import { XMLParser, XMLBuilder } from 'fast-xml-parser';
-import * as KemonoSample from '../../../assets/cs/kemono_example.json'
 
 @Component({
   selector: 'app-game-character-generate-window',
@@ -88,7 +87,175 @@ export class GameCharacterGenerateWindowComponent {
     }
   }
   appspot_kemono(charadata, download_flg){
-    let kemonosheet = KemonoSample;
+    let kemonosheet = null;
+    kemonosheet = {
+      "character": {
+        "data": {
+          "data": [
+            {
+              "data": {
+                "#text": "testCharacter_4_image",
+                "@_type": "image",
+                "@_name": "imageIdentifier"
+              },
+              "@_name": "image"
+            },
+            {
+              "data": [
+                {
+                    "#text": "仮名",
+                    "@_name": "name"
+                },
+                {
+                    "#text": 3,
+                    "@_name": "size"
+                }
+              ],
+              "@_name": "common"
+            },
+            {
+              "data": [
+                {
+                  "data": [
+                    {
+                      "#text": 10,
+                      "@_type": "numberResource",
+                      "@_currentValue": "10",
+                      "@_name": "余裕"
+                    },
+                    {
+                      "#text": 2,
+                      "@_type": "",
+                      "@_currentValue": "2",
+                      "@_name": "予算"
+                    },
+                    {
+                      "#text": "特性名",
+                      "@_name": "特性①",
+                      "@_type": ""
+                    },
+                    {
+                      "#text": "特性名",
+                      "@_name": "特性②",
+                      "@_type": ""
+                    },
+                    {
+                      "#text": 0,
+                      "@_name": "獸憑き",
+                      "@_type": ""
+                    },
+                    {
+                      "#text": "",
+                      "@_name": "状態異常"
+                    }
+                  ],
+                  "@_name": "リソース"
+                },
+                {
+                  "data": [
+                    {
+                      "#text": "本名:\n性別:　年齢:\n仮面:\n貌力:\n雰囲気:\n動機:",
+                      "@_type": "note",
+                      "@_name": "設定"
+                    },
+                    {
+                      "#text": 3,
+                      "@_name": "貌力の強度",
+                      "@_type": ""
+                    },
+                    {
+                      "#text": "「」(威力:)(特殊効果:)",
+                      "@_type": "",
+                      "@_name": "武器"
+                    },
+                    {
+                      "#text": "「」(軽減値:)(特殊効果:)",
+                      "@_name": "防具"
+                    },
+                    {
+                      "#text": "「」(特殊効果:)",
+                      "@_name": "小道具１"
+                    },
+                    {
+                      "#text": "「」(特殊効果:)",
+                      "@_name": "小道具２"
+                    },
+                    {
+                      "#text": "1.\n2.\n3.\n4.\n5.\n6.\n7.\n8.",
+                      "@_name": "持ち物",
+                      "@_type": "note"
+                    }
+                  ],
+                  "@_name": "情報"
+                },
+                {
+                  "data": [
+                    {
+                      "#text": 7,
+                      "@_name": "移動"
+                    },
+                    {
+                      "#text": 7,
+                      "@_name": "格闘"
+                    },
+                    {
+                      "#text": 7,
+                      "@_name": "射撃"
+                    },
+                    {
+                      "#text": 7,
+                      "@_name": "製作"
+                    },
+                    {
+                      "#text": 7,
+                      "@_name": "察知"
+                    },
+                    {
+                      "#text": 7,
+                      "@_name": "自制"
+                    }
+                  ],
+                  "@_name": "能力"
+                },
+                {
+                  "data": [
+                    {
+                      "#text": 5,
+                      "@_name": "日常",
+                      "@_type": "numberResource",
+                      "@_currentValue": "5"
+                    },
+                    {
+                      "#text": 5,
+                      "@_name": "日常",
+                      "@_type": "numberResource",
+                      "@_currentValue": "2"
+                    },
+                    {
+                      "#text": 5,
+                      "@_name": "日常",
+                      "@_type": "numberResource",
+                      "@_currentValue": "5"
+                    }
+                  ],
+                  "@_name": "絆"
+                }
+                ],
+                "@_name": "detail"
+            }
+          ],
+          "@_name": "character"
+        },
+        "chat-palette": {
+          "#text": "1d12\nKC\nKA{移動}  判定:移動\nKA{格闘}  判定:格闘\nKA{射撃}  判定:射撃\nKA{製作}  判定:製作\nKA{察知}  判定:察知\nKA{自制}  判定:自制\n1d12+{移動}  先制値決定:移動\n1d12+{察知}  先制値決定:察知\n1d12+{自制}  先制値決定:自制\nKA10-{負傷}  復帰判定\nFT 大失敗表",
+          "@_dicebot": "KemonoNoMori"
+        },
+        "@_location.name": "table"
+      }
+    };
+
+
+    let handlename = charadata.base.handlename;
 
     /// 使用する特性を確認 ///
     let talent = charadata.base.talent
@@ -123,12 +290,11 @@ export class GameCharacterGenerateWindowComponent {
     };
 
     character_note = `本名:${truename}
-    性別:${sex}　年齢:${age}
-    仮面:${face}
-    貌力:${facepower}
-    雰囲気:${atmosphere}
-    動機:${motivation}
-    `;
+性別:${sex}　年齢:${age}
+仮面:${face}
+貌力:${facepower}
+雰囲気:${atmosphere}
+動機:${motivation}`;
     /// ここまで ///
 
     let debuff = "";
@@ -137,42 +303,76 @@ export class GameCharacterGenerateWindowComponent {
     };
 
     /// 装備中の武器を取得 ///
-    interface Weapon {
-      damage: string;
-      equipment: string;
-      explain: string;
-      name: string;
-      range: string;
-    }
-    
     let weapons = charadata.weapons;
 
     let weaponName: string | null = null;
     let weaponDamage: string | null = null;
     let weaponRange: string | null = null;
-    let explain: string | null = null;
+    let weaponExplain: string = "";
 
-    
+    let equipmentWeapon: string | null = null;
 
     for (const weapon of weapons) {
       if (weapon.equipment === "装備") {
         weaponName = weapon.name;
         weaponDamage = weapon.damage;
         weaponRange = weapon.range;
-        explain = weapon.explain;
-        let equipmentWeapon: string = `「${weaponName}」(威力:${weaponDamage})(特殊効果:${explain})`
+        if (weapon.explain == null){
+          weaponExplain = "";
+        } else {
+          weaponExplain = weapon.explain;
+        }
         break; // 一つ取り出した時点で処理を終了
       };
     };
-    
-    
+    equipmentWeapon = `「${weaponName}」(威力:${weaponDamage})(特殊効果:${weaponExplain})`;
     /// ここまで ///
 
+    /// 装備中の防具を取得 ///
+    let armors = charadata.armors;
 
-    console.log(kemonosheet.character.data.data[2].data[0].data[1]["#text"])
+    let armorName: string | null = null;
+    let armorMitigation: string | null = null;
+    let armorExplain: string | null = null;
+
+    let equipmentArmor: string = `「${armorName}」(軽減値:${armorMitigation})(特殊効果:${armorExplain})`;
+
+    for (const armor of armors) {
+      if (armor.equipment === "装備") {
+        armorName = armor.name;
+        armorMitigation = armor.damage;
+        if (armor.explain == null){
+          armorExplain = "";
+        } else {
+          armorExplain = armor.explain;
+        }
+        break; // 一つ取り出した時点で処理を終了
+      }
+    }
+    equipmentArmor = `「${armorName}」(軽減値:${armorMitigation})(特殊効果:${armorExplain})`;
+    /// ここまで ///
+
+    /// 装備中の小道具を取得 ///
+    let accessories = charadata.accessories;
+    const equipmentAccessories: { name: string; explain: string | null }[] = [];
+    for (const accessory of accessories) {
+      if (accessory.equipment === "装備") {
+        if (accessory.explain == null){
+          accessory.explain = "";
+        }
+        equipmentAccessories.push({
+          name: accessory.name,
+          explain: accessory.explain,
+        });
+        if (equipmentAccessories.length >= 2) {
+          break; // 最大２つ取得したらループを終了
+        }
+      }
+    };
+    /// ここまで ///
 
     // 仮名
-    kemonosheet.character.data.data[1].data[0]["#text"] = charadata.base.handlename;
+    kemonosheet.character.data.data[1].data[0]["#text"] = handlename;
     // 余裕
     kemonosheet.character.data.data[2].data[0].data[0]["#text"] = charadata.status.margin.limit;
     kemonosheet.character.data.data[2].data[0].data[0]["@_currentValue"] = charadata.status.margin.limit;
@@ -189,34 +389,81 @@ export class GameCharacterGenerateWindowComponent {
     // キャラノート
     kemonosheet.character.data.data[2].data[1].data[0]["#text"] = character_note;
     // 貌力の強度
-    kemonosheet.character.data.data[2].data[1].data[1]['@_name'] = Object.keys(charadata.facepower).length;
+    kemonosheet.character.data.data[2].data[1].data[1]["@_name"] = Object.keys(charadata.facepower).length;
     // 武器
-    // kemonosheet
-
+    kemonosheet.character.data.data[2].data[1].data[2]["#text"] = equipmentWeapon;
+    // 防具
+    kemonosheet.character.data.data[2].data[1].data[3]["#text"] = equipmentArmor;
+    // 小道具
+    for (let i = 0; i <= 1; i++) {
+      if(equipmentAccessories[i]){
+        kemonosheet.character.data.data[2].data[1].data[i+4]["#text"] = `「${equipmentAccessories[i].name}」(特殊効果:${equipmentAccessories[i].explain})`;
+      }
+    };
+    /// 能力値
+    let ability = charadata.base.ability;
+    // 移動
+    kemonosheet.character.data.data[2].data[2].data[0]['#text'] = ability.move;
+    // 格闘
+    kemonosheet.character.data.data[2].data[2].data[1]['#text'] = ability.fight;
+    // 射撃
+    kemonosheet.character.data.data[2].data[2].data[2]['#text'] = ability.shooting;
+    // 製作
+    kemonosheet.character.data.data[2].data[2].data[3]['#text'] = ability.create;
+    // 察知
+    kemonosheet.character.data.data[2].data[2].data[4]['#text'] = ability.awareness;
+    // 自制
+    kemonosheet.character.data.data[2].data[2].data[5]['#text'] = ability.restraint;
+    // 日常
+    for (let i = 0; i <= 2; i++){
+      kemonosheet.character.data.data[2].data[3].data[i]["@_name"] = charadata.days[i].name;
+      kemonosheet.character.data.data[2].data[3].data[i]["#text"] = charadata.days[i].level;
+      kemonosheet.character.data.data[2].data[3].data[i]["@_currentValue"] = charadata.days[i].current;
+    }
+    // 仲間
+    let friends = charadata.friends;
+    for (let friend of friends) {
+      let data = {
+        "@_name": friend.name,
+        "#text": friend.level,
+        "@_type": "numberResource",
+        "@_currentValue": friend.current
+      };
+      kemonosheet.character.data.data[2].data[3].data.push(data);
+    };
 
     let summary = `<?xml version="1.0" encoding="UTF-8"?>
     <summary-setting sortTag="name" sortOrder="ASC" dataTag="開始条件　展開 耐久度　余裕　予算　威力　軽減値 特性①　特性② 　特殊効果　異形　獸憑き　状態異常 　移動　格闘　射撃　製作　察知　自制　 貌力　装備 武器　防具　小道具　持ち物"></summary-setting>
     `
 
+    const xb = new XMLBuilder({
+      ignoreAttributes: false,
+      textNodeName: "#text",
+      attributeNamePrefix: "@_",
+      format: true,
+      cdataPropName: "__cdata",
+    });
 
+    const xmlContent = xb.build(kemonosheet);
+    console.log(xmlContent)
 
     switch(download_flg){
       case false:
-        // let xmlElement: Element = XmlUtil.xml2element(summary);
-        // if (xmlElement) EventSystem.trigger('XML_LOADED', { xmlElement: xmlElement });
-        // xmlElement = XmlUtil.xml2element(kemonocs);
-        // if (xmlElement) EventSystem.trigger('XML_LOADED', { xmlElement: xmlElement });
+        let xmlElement: Element = XmlUtil.xml2element(summary);
+        if (xmlElement) EventSystem.trigger('XML_LOADED', { xmlElement: xmlElement });
+        xmlElement = XmlUtil.xml2element(xmlContent);
+        if (xmlElement) EventSystem.trigger('XML_LOADED', { xmlElement: xmlElement });
         break;
       case true:
-        // const zip = new JSZip();
-        // zip.file(`${name}.xml`, kemonocs);
-        // zip.file('summary.xml', summary);
-        // zip.generateAsync({ type: 'blob' }).then((blob) => {
-        //   const downloadLink = document.createElement('a');
-        //   downloadLink.href = URL.createObjectURL(blob);
-        //   downloadLink.download = `${name}.zip`;
-        //   downloadLink.click();
-        // });
+        const zip = new JSZip();
+        zip.file(`${handlename}.xml`, xmlContent);
+        zip.file('summary.xml', summary);
+        zip.generateAsync({ type: 'blob' }).then((blob) => {
+          const downloadLink = document.createElement('a');
+          downloadLink.href = URL.createObjectURL(blob);
+          downloadLink.download = `${handlename}.zip`;
+          downloadLink.click();
+        });
         break;
     };
   };
