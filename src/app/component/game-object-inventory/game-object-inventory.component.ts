c22b1e5d4ab0f64ba46139a4edd93caa0988616b
mrrlll <mxrs2d@gmail.com>
2023-12-05 14:49:46 +0900

    修正漏れ＆音がする必要ない。

--- src/app/component/game-object-inventory/game-object-inventory.component.ts
+++ src/app/component/game-object-inventory/game-object-inventory.component.ts
@@ -228,13 +228,11 @@ export class GameObjectInventoryComponent implements OnInit, OnDestroy {
       ? {
         name: '☑ 他のキャラクターに乗る', action: () => {
           gameObject.isNotRide = true;
-          SoundEffect.play(PresetSound.sweep);
           EventSystem.trigger('UPDATE_INVENTORY', null);
         }
       } : {
         name: '☐ 他のキャラクターに乗る', action: () => {
           gameObject.isNotRide = false;
-          SoundEffect.play(PresetSound.piecePut);
           EventSystem.trigger('UPDATE_INVENTORY', null);
         }
       }));
