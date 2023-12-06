508bd213cf4ee0db00334cc2fa920e84ad3b7f79
mrrlll <mxrs2d@gmail.com>
2023-12-06 17:50:13 +0900

    ダイス表の判定とメッセージ処理を追加

--- src/app/class/dice-bot.ts
+++ src/app/class/dice-bot.ts
@@ -10,6 +10,8 @@ import { ObjectStore } from './core/synchronize-object/object-store';
 import { EventSystem } from './core/system';
 import { PromiseQueue } from './core/system/util/promise-queue';
 import { StringUtil } from './core/system/util/string-util';
+import { DiceTable } from './dice-table';
+import { DiceTablePalette } from './chat-palette';
 
 interface DiceRollResult {
   id: string;
@@ -35,6 +37,10 @@ let queue: PromiseQueue = initializeDiceBotQueue();
 @SyncObject('dice-bot')
 export class DiceBot extends GameObject {
   static diceBotInfos: GameSystemInfo[] = [];
+
+  getDiceTables(): DiceTable[] {
+    return ObjectStore.instance.getObjects(DiceTable);
+  }
 
   // GameObject Lifecycle
   onStoreAdded() {
@@ -70,6 +76,71 @@ export class DiceBot extends GameObject {
         if (!rollResult.result) return;
         console.log(rollResult)
         this.sendResultMessage(rollResult, chatMessage);
+        return;
+      })
+      .on('DICE_TABLE_MESSAGE', async event => {
+        console.log('ダイス表判定');
+
+        let chatMessage = ObjectStore.instance.get<ChatMessage>(event.data.messageIdentifier);
+        if (!chatMessage || !chatMessage.isSendFromSelf || chatMessage.isSystem) return;
+
+        let text: string = StringUtil.toHalfWidth(chatMessage.text);
+        let splitText = text.split(/\s/);
+        let gameType: string = chatMessage.tag;
+
+        let diceTable = this.getDiceTables() ;
+        if( !diceTable )return;
+        if( splitText.length == 0 )return;
+
+        console.log('コマンド候補:' + splitText[0] );
+
+        let rollDice = null ;
+        let rollTable = null;
+        for( let table of diceTable){
+          if( table.command == splitText[0] ){
+            rollTable = table;
+          }
+        }
+        if( !rollTable ) return;
+
+        try {
+          let regArray = /^((\d+)?\s+)?([^\s]*)?/ig.exec(rollTable.dice);
+          let repeat: number = (regArray[2] != null) ? Number(regArray[2]) : 1;
+          let rollText: string = (regArray[3] != null) ? regArray[3] : text;
+          let finalResult: DiceRollResult = { id: 'DiceBot', result: '', isSecret: false };
+          for (let i = 0; i < repeat && i < 32; i++) {
+            let rollResult = await DiceBot.diceRollAsync(rollText, gameType);
+            if (rollResult.result.length < 1) break;
+
+            finalResult.result += rollResult.result;
+            finalResult.isSecret = finalResult.isSecret || rollResult.isSecret;
+            if (1 < repeat) finalResult.result += ` #${i + 1}`;
+          }
+          console.log('finalResult.result:' + finalResult.result );
+
+          let rolledDiceNum = finalResult.result.match(/\d+$/);
+          let tableAns = "ダイス目の番号が表にありません";
+          if( rolledDiceNum ){
+            console.log('rolledDiceNum:' + rolledDiceNum[0] );
+
+            let tablePalette = rollTable.diceTablePalette.getPalette();
+              console.log('tablePalette:' + tablePalette );
+            for( let i in tablePalette ){
+              console.log('oneTable:' + tablePalette[i] );
+
+              let splitOneTable = tablePalette[i].split(/[:：,，\s]/);
+              if( splitOneTable[0] == rolledDiceNum[0] ){
+                tableAns = tablePalette[i];
+              }
+            }
+
+          }
+          finalResult.result += '\n'+tableAns;
+          this.sendResultMessage(finalResult , chatMessage);
+
+        } catch (e) {
+          console.error(e);
+        }
         return;
       });
   }
