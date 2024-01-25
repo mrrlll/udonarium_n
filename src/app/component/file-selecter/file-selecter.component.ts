91479ea2017ec21245e6efa1b5e146cc9951d4a4
mrrlll <mxrs2d@gmail.com>
2024-01-25 18:10:59 +0900

    ドラッグ禁止

--- src/app/component/file-selecter/file-selecter.component.ts
+++ src/app/component/file-selecter/file-selecter.component.ts
@@ -292,6 +292,7 @@ export class FileSelecterComponent implements OnInit, OnDestroy, AfterViewInit {
 
   chanageImageView(imageFile: ImageFile) {
     if (imageFile.state === ImageState.COMPLETE) {
+      if (AppComponent.imageUrl) URL.revokeObjectURL(AppComponent.imageUrl);
       AppComponent.imageUrl = URL.createObjectURL(imageFile.blob);
     } else {
       AppComponent.imageUrl = imageFile.url;
